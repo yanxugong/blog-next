56e5a56c9463480bb3b35e9ad6cfef1e0c0ffb95
yanxugong <35914225+yanxugong@users.noreply.github.com>
2022-06-06 02:21:36 +0000

    Deploying to gh-pages from  @ 03126b9c0dd428a3a3f7ca5253742f7b182f124c 🚀

--- assets/study_vue3_responsive-system.md.f0bff472.js
+++ assets/study_vue3_responsive-system.md.f0bff472.js
@@ -1,74 +1,57 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Yanxu Gong's Blog</title>
-    <meta name="description" content="Personal blog based on Github Action + VitePress">
-    <link rel="stylesheet" href="/blog-next/assets/style.88a1b92e.css">
-    <link rel="modulepreload" href="/blog-next/assets/chunks/AlgoliaSearchBox.dd75b3aa.js">
-    <link rel="modulepreload" href="/blog-next/assets/app.acdedced.js">
-    <link rel="modulepreload" href="/blog-next/assets/study_vue3_responsive-system.md.f0bff472.lean.js">
-    
-    <link rel="icon" type="image/svg+xml" href="/logo.svg">
-  <meta name="twitter:title" content="Yanxu Gong&#39;s Blog">
-  <meta property="og:title" content="Yanxu Gong&#39;s Blog">
-  </head>
-  <body>
-    <div id="app"><!--[--><div class="theme"><header class="nav-bar" data-v-95cd145e><div class="sidebar-button" data-v-95cd145e><svg class="icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" viewBox="0 0 448 512"><path fill="currentColor" d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z" class></path></svg></div><a class="nav-bar-title" href="/blog-next/" aria-label="Yanxu Gong&#39;s Blog, back to home" data-v-95cd145e data-v-2627e0f1><img class="logo" src="/blog-next/logo.svg" alt="Logo" data-v-2627e0f1> Yanxu Gong&#39;s Blog</a><div class="flex-grow" data-v-95cd145e></div><div class="nav" data-v-95cd145e><nav class="nav-links" data-v-95cd145e data-v-b68e4bf6><!--[--><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item active" href="/blog-next/study/" data-v-43e5dab6>学习 <!----></a></div></div><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item" href="/blog-next/photography/" data-v-43e5dab6>摄影 <!----></a></div></div><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item" href="/blog-next/emotion/" data-v-43e5dab6>情感 <!----></a></div></div><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item" href="/blog-next/about/" data-v-43e5dab6>关于 <!----></a></div></div><!--]--><!----><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item isExternal" href="https://github.com/yanxugong/blog-next" target="_blank" rel="noopener noreferrer" data-v-43e5dab6>GitHub <svg class="icon outbound" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewbox="0 0 100 100" width="15" height="15" data-v-43e5dab6><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg></a></div></div></nav></div><!--[--><!--[--><div class="algolia-search-box" id="docsearch"></div><!--]--><!--]--></header><aside class="sidebar" data-v-1c2cc348><nav class="nav-links nav" data-v-1c2cc348 data-v-b68e4bf6><!--[--><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item active" href="/blog-next/study/" data-v-43e5dab6>学习 <!----></a></div></div><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item" href="/blog-next/photography/" data-v-43e5dab6>摄影 <!----></a></div></div><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item" href="/blog-next/emotion/" data-v-43e5dab6>情感 <!----></a></div></div><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item" href="/blog-next/about/" data-v-43e5dab6>关于 <!----></a></div></div><!--]--><!----><div class="item" data-v-b68e4bf6><div class="nav-link" data-v-b68e4bf6 data-v-43e5dab6><a class="item isExternal" href="https://github.com/yanxugong/blog-next" target="_blank" rel="noopener noreferrer" data-v-43e5dab6>GitHub <svg class="icon outbound" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewbox="0 0 100 100" width="15" height="15" data-v-43e5dab6><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg></a></div></div></nav><!--[--><!--]--><!----><!--[--><!--]--></aside><div class="sidebar-mask"></div><main class="page" data-v-3d00e640><div class="container" data-v-3d00e640><!--[--><!--]--><div style="position:relative;" class="content" data-v-3d00e640><div><p>响应式数据基本实现：</p><div class="language-js"><pre><code><span class="token comment">// 存储副作用函数的桶</span>
+import{_ as n,c as s,o as a,d as t}from"./app.acdedced.js";const d='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"study/vue3/responsive-system.md"}',p={},o=t(`<p>\u54CD\u5E94\u5F0F\u6570\u636E\u57FA\u672C\u5B9E\u73B0\uFF1A</p><div class="language-js"><pre><code><span class="token comment">// \u5B58\u50A8\u526F\u4F5C\u7528\u51FD\u6570\u7684\u6876</span>
 <span class="token keyword">const</span> bucket <span class="token operator">=</span> <span class="token keyword">new</span> <span class="token class-name">Set</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
 
-<span class="token comment">// 原始数据</span>
+<span class="token comment">// \u539F\u59CB\u6570\u636E</span>
 <span class="token keyword">const</span> data <span class="token operator">=</span> <span class="token punctuation">{</span> <span class="token literal-property property">text</span><span class="token operator">:</span> <span class="token string">&quot;hello world&quot;</span> <span class="token punctuation">}</span><span class="token punctuation">;</span>
-<span class="token comment">// 对原始数据的代理</span>
+<span class="token comment">// \u5BF9\u539F\u59CB\u6570\u636E\u7684\u4EE3\u7406</span>
 <span class="token keyword">const</span> obj <span class="token operator">=</span> <span class="token keyword">new</span> <span class="token class-name">Proxy</span><span class="token punctuation">(</span>data<span class="token punctuation">,</span> <span class="token punctuation">{</span>
-  <span class="token comment">// 拦截读取操作</span>
+  <span class="token comment">// \u62E6\u622A\u8BFB\u53D6\u64CD\u4F5C</span>
   <span class="token function">get</span><span class="token punctuation">(</span>target<span class="token punctuation">,</span> key<span class="token punctuation">)</span> <span class="token punctuation">{</span>
-    <span class="token comment">// 将副作用函数 effect 添加到存储副作用函数的桶中</span>
+    <span class="token comment">// \u5C06\u526F\u4F5C\u7528\u51FD\u6570 effect \u6DFB\u52A0\u5230\u5B58\u50A8\u526F\u4F5C\u7528\u51FD\u6570\u7684\u6876\u4E2D</span>
     bucket<span class="token punctuation">.</span><span class="token function">add</span><span class="token punctuation">(</span>effect<span class="token punctuation">)</span><span class="token punctuation">;</span>
-    <span class="token comment">// 返回属性值</span>
+    <span class="token comment">// \u8FD4\u56DE\u5C5E\u6027\u503C</span>
     <span class="token keyword">return</span> target<span class="token punctuation">[</span>key<span class="token punctuation">]</span><span class="token punctuation">;</span>
   <span class="token punctuation">}</span><span class="token punctuation">,</span>
-  <span class="token comment">// 拦截设置操作</span>
+  <span class="token comment">// \u62E6\u622A\u8BBE\u7F6E\u64CD\u4F5C</span>
   <span class="token function">set</span><span class="token punctuation">(</span>target<span class="token punctuation">,</span> key<span class="token punctuation">,</span> newVal<span class="token punctuation">)</span> <span class="token punctuation">{</span>
-    <span class="token comment">// 设置属性值</span>
+    <span class="token comment">// \u8BBE\u7F6E\u5C5E\u6027\u503C</span>
     target<span class="token punctuation">[</span>key<span class="token punctuation">]</span> <span class="token operator">=</span> newVal<span class="token punctuation">;</span>
-    <span class="token comment">// 把副作用函数从桶里取出并执行</span>
+    <span class="token comment">// \u628A\u526F\u4F5C\u7528\u51FD\u6570\u4ECE\u6876\u91CC\u53D6\u51FA\u5E76\u6267\u884C</span>
     bucket<span class="token punctuation">.</span><span class="token function">forEach</span><span class="token punctuation">(</span><span class="token punctuation">(</span><span class="token parameter">fn</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token function">fn</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
-    <span class="token comment">// 返回 true 代表设置操作成功</span>
+    <span class="token comment">// \u8FD4\u56DE true \u4EE3\u8868\u8BBE\u7F6E\u64CD\u4F5C\u6210\u529F</span>
     <span class="token keyword">return</span> <span class="token boolean">true</span><span class="token punctuation">;</span>
   <span class="token punctuation">}</span><span class="token punctuation">,</span>
 <span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
 
-<span class="token comment">// 副作用函数</span>
+<span class="token comment">// \u526F\u4F5C\u7528\u51FD\u6570</span>
 <span class="token keyword">function</span> <span class="token function">effect</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
   document<span class="token punctuation">.</span>body<span class="token punctuation">.</span>innerText <span class="token operator">=</span> obj<span class="token punctuation">.</span>text<span class="token punctuation">;</span>
 <span class="token punctuation">}</span>
-<span class="token comment">// 执行副作用函数，触发读取</span>
+<span class="token comment">// \u6267\u884C\u526F\u4F5C\u7528\u51FD\u6570\uFF0C\u89E6\u53D1\u8BFB\u53D6</span>
 <span class="token function">effect</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
-<span class="token comment">// 1 秒后修改响应式数据</span>
+<span class="token comment">// 1 \u79D2\u540E\u4FEE\u6539\u54CD\u5E94\u5F0F\u6570\u636E</span>
 <span class="token function">setTimeout</span><span class="token punctuation">(</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span>
   obj<span class="token punctuation">.</span>text <span class="token operator">=</span> <span class="token string">&quot;hello vue3&quot;</span><span class="token punctuation">;</span>
 <span class="token punctuation">}</span><span class="token punctuation">,</span> <span class="token number">1000</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
-</code></pre></div><p>设计一个完善的响应系统：</p><div class="language-js"><pre><code><span class="token comment">// 存储副作用函数的桶</span>
+</code></pre></div><p>\u8BBE\u8BA1\u4E00\u4E2A\u5B8C\u5584\u7684\u54CD\u5E94\u7CFB\u7EDF\uFF1A</p><div class="language-js"><pre><code><span class="token comment">// \u5B58\u50A8\u526F\u4F5C\u7528\u51FD\u6570\u7684\u6876</span>
 <span class="token keyword">const</span> bucket <span class="token operator">=</span> <span class="token keyword">new</span> <span class="token class-name">Set</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
 
-<span class="token comment">// 原始数据</span>
+<span class="token comment">// \u539F\u59CB\u6570\u636E</span>
 <span class="token keyword">const</span> data <span class="token operator">=</span> <span class="token punctuation">{</span> <span class="token literal-property property">text</span><span class="token operator">:</span> <span class="token string">&quot;hello world&quot;</span> <span class="token punctuation">}</span><span class="token punctuation">;</span>
 
-<span class="token comment">// 用一个全局变量存储被注册的副作用函数</span>
+<span class="token comment">// \u7528\u4E00\u4E2A\u5168\u5C40\u53D8\u91CF\u5B58\u50A8\u88AB\u6CE8\u518C\u7684\u526F\u4F5C\u7528\u51FD\u6570</span>
 <span class="token keyword">let</span> activeEffect<span class="token punctuation">;</span>
-<span class="token comment">// effect 函数用于注册副作用函数</span>
+<span class="token comment">// effect \u51FD\u6570\u7528\u4E8E\u6CE8\u518C\u526F\u4F5C\u7528\u51FD\u6570</span>
 <span class="token keyword">function</span> <span class="token function">effect</span><span class="token punctuation">(</span><span class="token parameter">fn</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
-  <span class="token comment">// 当调用 effect 注册副作用函数时，将副作用函数 fn 赋值给 activeEffect</span>
+  <span class="token comment">// \u5F53\u8C03\u7528 effect \u6CE8\u518C\u526F\u4F5C\u7528\u51FD\u6570\u65F6\uFF0C\u5C06\u526F\u4F5C\u7528\u51FD\u6570 fn \u8D4B\u503C\u7ED9 activeEffect</span>
   activeEffect <span class="token operator">=</span> fn<span class="token punctuation">;</span>
-  <span class="token comment">// 执行副作用函数</span>
+  <span class="token comment">// \u6267\u884C\u526F\u4F5C\u7528\u51FD\u6570</span>
   <span class="token function">fn</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
 <span class="token punctuation">}</span>
 
 <span class="token keyword">const</span> obj <span class="token operator">=</span> <span class="token keyword">new</span> <span class="token class-name">Proxy</span><span class="token punctuation">(</span>data<span class="token punctuation">,</span> <span class="token punctuation">{</span>
   <span class="token function">get</span><span class="token punctuation">(</span>target<span class="token punctuation">,</span> key<span class="token punctuation">)</span> <span class="token punctuation">{</span>
-    <span class="token comment">// 将 activeEffect 中存储的副作用函数收集到 “桶” 中</span>
+    <span class="token comment">// \u5C06 activeEffect \u4E2D\u5B58\u50A8\u7684\u526F\u4F5C\u7528\u51FD\u6570\u6536\u96C6\u5230 \u201C\u6876\u201D \u4E2D</span>
     <span class="token keyword">if</span> <span class="token punctuation">(</span>activeEffect<span class="token punctuation">)</span> <span class="token punctuation">{</span>
       bucket<span class="token punctuation">.</span><span class="token function">add</span><span class="token punctuation">(</span>activeEffect<span class="token punctuation">)</span><span class="token punctuation">;</span>
     <span class="token punctuation">}</span>
@@ -82,7 +65,7 @@
 <span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
 
 <span class="token function">effect</span><span class="token punctuation">(</span>
-  <span class="token comment">// 一个匿名的副作用函数</span>
+  <span class="token comment">// \u4E00\u4E2A\u533F\u540D\u7684\u526F\u4F5C\u7528\u51FD\u6570</span>
   <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span>
     console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span><span class="token string">&quot;effect run&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
     document<span class="token punctuation">.</span>body<span class="token punctuation">.</span>innerText <span class="token operator">=</span> obj<span class="token punctuation">.</span>text<span class="token punctuation">;</span>
@@ -90,12 +73,7 @@
 <span class="token punctuation">)</span><span class="token punctuation">;</span>
 
 <span class="token function">setTimeout</span><span class="token punctuation">(</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span>
-  <span class="token comment">// 副作用函数中并没有读取 notExist 属性的值</span>
+  <span class="token comment">// \u526F\u4F5C\u7528\u51FD\u6570\u4E2D\u5E76\u6CA1\u6709\u8BFB\u53D6 notExist \u5C5E\u6027\u7684\u503C</span>
   obj<span class="token punctuation">.</span>notExist <span class="token operator">=</span> <span class="token string">&quot;hello vue3&quot;</span><span class="token punctuation">;</span>
 <span class="token punctuation">}</span><span class="token punctuation">,</span> <span class="token number">1000</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
-</code></pre></div></div></div><footer class="page-footer" data-v-3d00e640 data-v-0d8328fe><div class="edit" data-v-0d8328fe><div class="edit-link" data-v-0d8328fe data-v-f484245e><a class="link" href="https://github.com/yanxugong/blog-next/edit/main/study/vue3/responsive-system.md" target="_blank" rel="noopener noreferrer" data-v-f484245e>为此页提供修改建议 <svg class="icon outbound icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewbox="0 0 100 100" width="15" height="15" data-v-f484245e><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg></a></div></div><div class="updated" data-v-0d8328fe><!----></div></footer><!----><!--[--><!--]--></div></main></div><!----><!--]--></div>
-    <script>__VP_HASH_MAP__ = JSON.parse("{\"about_index.md\":\"c8fb677e\",\"emotion_index.md\":\"9df12840\",\"index.md\":\"122593cd\",\"photography_index.md\":\"8ab54a43\",\"study_algorithm_linked-list.md\":\"56de5a22\",\"study_engineering_vite.md\":\"a5e68948\",\"study_engineering_webpack-hmr.md\":\"fd9d7b24\",\"study_engineering_webpack-loader.md\":\"c44e1d7e\",\"study_engineering_webpack-main.md\":\"c92b7e90\",\"study_engineering_webpack-plugin.md\":\"b64db039\",\"study_frame_deep-react-one.md\":\"9567db07\",\"study_frame_deep-react-two.md\":\"647e3106\",\"study_index.md\":\"24d0e116\",\"study_js-basis_implementation-mechanism.md\":\"5724d96f\",\"study_js-basis_prototype-and-prototype-chain.md\":\"dffe13ab\",\"study_js-basis_scope-and-closure.md\":\"128bae09\",\"study_js-basis_syntax-and-api.md\":\"53be698c\",\"study_js-basis_variables-and-types.md\":\"9fcfaf7a\",\"study_js-basis_you-do-not-konw-js-down.md\":\"96afbde7\",\"study_js-basis_you-do-not-konw-js.md\":\"646fef6b\",\"study_network_from-url-to-page-render.md\":\"ffd09749\",\"study_performance-optimization_first-screen.md\":\"0aa2c624\",\"study_server_go-getting-started.md\":\"ce44aeeb\",\"study_vue3_responsive-system.md\":\"f0bff472\"}")</script>
-    <script type="module" async src="/blog-next/assets/app.acdedced.js"></script>
-    
-  </body>
-</html>
+</code></pre></div>`,4),c=[o];function e(u,l,k,i,r,f){return a(),s("div",null,c)}var y=n(p,[["render",e]]);export{d as __pageData,y as default};
