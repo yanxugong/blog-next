39fcab27e700354e4824002baa23e9b9dfd76e13
antyonygong@tencent.com <antyonygong@tencent.com>
2022-07-18 12:40:48 +0800

    fix: 修正edit页面跳转地址

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
 
     editLink: {
-      pattern: "https://github.com/vuejs/vitepress/edit/main/docs/:path",
+      pattern: "https://github.com/yanxugong/blog-next/edit/main/docs/:path",
       text: "Edit this page on GitHub",
     },
 
